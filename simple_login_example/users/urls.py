1e05a132da899bbdb2bb81b2f7c2d962ed011ad8
2018079116 <wjdwlstjd968@hanyang.ac.kr>
2022-12-03 12:01:37 +0900

    .

--- simple_login_example/users/urls.py
+++ simple_login_example/users/urls.py
@@ -5,5 +5,8 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.login, name='login'),  # users/login/,
-    path('login/<int:id>/', views.login_detail, name='login-detail')
+    path('login/<int:id>/', views.login_detail, name='login-detail'),
+    path('login/index', views.index, name='index'),
+    path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup')
 ]
